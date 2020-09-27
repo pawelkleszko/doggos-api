3de45e9638d479b28ac5c67d95cb70c8a62312f4
pawelkleszko <kleszkopawel@gmail.com>
2020-09-27 13:50:08 +0200

    app functionality enable

--- js/app.js
+++ js/app.js
@@ -1,27 +1,87 @@
-const imgEl = document.querySelector('.doggos__img img');
+class Doggos {
+    constructor() {
+        this.imgEl = document.querySelector('.doggos__img img');
+        this.apiUrl = 'https://dog.ceo/api/';
+        this.btnsBreeds = document.querySelector('.buttons__container');
 
-const getRandomDoggoImg = () => {
+        this.init()
+    }
 
-    fetch('https://dog.ceo/api/breeds/image/random')
-        .then(res => res.json())
-        .then(res => console.log(res.message))
-        .catch(err => console.log('Coś poszło nie tak!', err));
+    getRandomDoggoImg() {
+
+        return fetch(`${this.apiUrl}breeds/image/random`)
+            .then(res => res.json())
+            .then(data => data.message)
+            .catch(err => console.log('Coś poszło nie tak!', err));
+    }
+
+    getDoggoImgByBreed(breed) {
+        return fetch(`${this.apiUrl}breed/${breed}/images/random`)
+            .then(res => res.json())
+            .then(res => res.message)
+            .catch(err => console.log('Coś poszło nie tak!', err));
+    }
+
+    getDoggosBreedList() {
+        return fetch(`${this.apiUrl}breeds/list/all`)
+            .then(res => res.json())
+            .then(res => res.message)
+            .catch(err => console.log('Coś poszło nie tak!', err));
+    }
+
+    showDoggo(img) {
+        this.imgEl.setAttribute('src', img);
+    }
+
+    init() {
+        this.getRandomDoggoImg()
+            .then(img => this.showDoggo(img))
+
+        this.showBreedButtons();
+    }
+
+    addBreed(breed, subBreed) {
+        console.log(breed);
+        let name;
+        let type;
+        if (typeof subBreed === 'undefined') {
+            name = breed;
+            type = breed;
+        } else {
+            name = `${breed} ${subBreed}`;
+            type = `${breed}/${subBreed}`;
+        }
+
+        const btn = document.createElement('button');
+        btn.classList.add('buttons__btn');
+        btn.innerHTML = name;
+        btn.addEventListener('click', () => {
+            window.scrollTo(0, 0);
+            this.getDoggoImgByBreed(type)
+                .then(img => this.showDoggo(img))
+        })
+
+        this.btnsBreeds.appendChild(btn)
+    }
+
+    showBreedButtons() {
+        this.getDoggosBreedList()
+            .then(breeds => {
+                for (const breed in breeds) {
+                    if (breeds[breed] === 0) {
+                        this.addBreed(breed)
+                    } else {
+                        for (const subBreed of breeds[breed]) {
+                            this.addBreed(breed, subBreed);
+                        }
+                    }
+                }
+            })
+    }
+
+
 }
 
-const getDoggoImgByBreed = (breed) => {
-    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-        .then(res => res.json())
-        .then(res => console.log(res.message))
-        .catch(err => console.log('Coś poszło nie tak!', err));
-}
-
-const getDoggosBreedList = () => {
-    fetch(`https://dog.ceo/api/breeds/list/all`)
-        .then(res => res.json())
-        .then(res => console.log(res.message))
-        .catch(err => console.log('Coś poszło nie tak!', err));
-}
-
-getRandomDoggoImg()
-getDoggoImgByBreed('boxer')
-getDoggosBreedList()
+document.addEventListener('DOMContentLoaded', () => {
+    new Doggos();
+});
